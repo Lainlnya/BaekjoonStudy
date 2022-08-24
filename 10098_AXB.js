cf12bcc756c041011e334bc48e914824517165e6
Lainlnya <kimyelim100@gmail.com>
2022-08-24 22:22:54 +0900

    Backjoon NO.1008, NO.1330, NO.10098

---
+++ 10098_AXB.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    input = line.split(' ').map(el => parseInt(el));
+    rl.close();
+});
+
+rl.on("close", () => {
+    console.log(input[0] * input[1]);
+    process.exit();
+});
---
+++ 1330.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    input = line.split(' ').map(el => parseInt(el));
+    rl.close();
+});
+
+rl.on("close", () => {
+    if (input[0] > input[1]) console.log(">");
+    else if (input[0] < input[1]) console.log("<");
+    else if (input[0] == input[1]) console.log("==");
+
+    process.exit();
+})

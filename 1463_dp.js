424df271bf32b97cebd2c5b63c00aa636354151c
Lainlnya <kimyelim100@gmail.com>
2023-04-06 21:24:32 +0900

    solving problem type dp

--- 1463_dp.js
+++ 1463_dp.js
@@ -1,27 +1,26 @@
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on("line", (line) => {
-    input = parseInt(line);
-    rl.close();
+rl.on('line', (line) => {
+  input = parseInt(line);
+  rl.close();
 });
 
-rl.on("close", () => {
-    const dp = new Array(input + 1).fill(0);
+rl.on('close', () => {
+  const dp = new Array(input + 1).fill(0);
 
-    for (let i = 2; i < dp.length; i++) {
-        dp[i] = dp[i - 1] + 1;
+  for (let i = 2; i < dp.length; i++) {
+    dp[i] = dp[i - 1] + 1;
 
-        if (i % 3 === 0) {
-            dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-        }
-
-        if (i % 2 === 0) {
-            dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-        }
+    if (i % 3 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
     }
-    console.log(dp[input]);
+
+    if (i % 2 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    }
+  }
 });

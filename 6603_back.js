691a3d4bc2a4d09bb2b4acfce8f753b319848a15
Lainlnya <kimyelim100@gmail.com>
2023-05-22 15:27:11 +0900

    solving problems type back-tracking

---
+++ 6603_back.js
@@ -0,0 +1,46 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+rl.on('line', (line) => {
+  input.push(line.split(' ').map(Number));
+});
+
+rl.on('close', () => {
+  input.pop();
+  for (let i = 0; i < input.length; i++) {
+    input[i].shift();
+  }
+  console.log(solution(input));
+});
+
+function solution(num_array) {
+  let output = [];
+  let answer = '';
+  function getCombination(arr, data, s, idx, r) {
+    if (s == r) {
+      answer += `${data.join(' ')}\n`;
+      return;
+    }
+    for (let i = idx; arr.length - i >= r - s; i++) {
+      data[s] = arr[i];
+      getCombination(arr, data, s + 1, i + 1, r);
+    }
+  }
+
+  for (let i = 0; i < num_array.length; i++) {
+    getCombination(num_array[i], output, 0, 0, 6);
+    answer += '\n';
+  }
+  return answer;
+}
+
+// console.log(
+//   solution([
+//     [1, 2, 3, 4, 5, 6, 7],
+//     [1, 2, 3, 5, 8, 13, 21, 34],
+//   ])
+// );
